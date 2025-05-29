aecfcd436d49c2f36c800092659810a16117ba09
Fábio Santos <fabiotrabmat@gmail.com>
2025-05-29 15:59:51 -0300

    fix: ajuste nos tipos de tweet e user nas respostas do servidor

--- client/src/services/types/tweet.ts
+++ client/src/services/types/tweet.ts
@@ -1,10 +1,14 @@
-import type { Tweet } from "../../types/tweet";
+import type { Tweet, TweetStatisticsFromServer } from "../../types/tweet";
 import type { ApiBaseResponse } from "./apiBaseResponse";
 
 export interface TweetListResponse extends ApiBaseResponse {
-  data: Tweet[];
+  data: Tweet[] | null;
 }
 
 export interface TweetResponse extends ApiBaseResponse {
-  data: Tweet;
+  data: Tweet | null;
+}
+
+export interface TweetStatisticsResponse extends ApiBaseResponse {
+  data: TweetStatisticsFromServer | null;
 }
--- client/src/services/types/user.ts
+++ client/src/services/types/user.ts
@@ -2,13 +2,13 @@ import type { User, UserResponseLogin } from "../../types/user";
 import type { ApiBaseResponse } from "./apiBaseResponse";
 
 export interface UserResponse extends ApiBaseResponse {
-  data: User;
+  data: User | null;
 }
 
 export interface UserListResponse extends ApiBaseResponse {
-  data: User[];
+  data: User[] | null;
 }
 
 export interface UserLoginServerResponse extends ApiBaseResponse {
-  data: UserResponseLogin;
+  data: UserResponseLogin | null;
 }
