252983d063145fbf2b80c0ab7e1a83bd8b0ec781
Fábio Santos <fabiotrabmat@gmail.com>
2025-05-29 16:08:24 -0300

    fix: limpa no componente Users usado para teste até este momento

--- client/src/pages/users/index.tsx
+++ client/src/pages/users/index.tsx
@@ -1,51 +1,12 @@
-import { useEffect, type ReactNode } from "react";
-import { CircleLoader } from "react-spinners";
-
-import { useGetTweetsQuery } from "../../services/tweetApiSlice";
-import { useAppDispatch } from "../../store/hooks";
-import { setTweets } from "../../store/reducers/tweetSlice";
+import { TweetForm } from "../../components/TweetForm";
+import { Login } from "../Login";
 
 export const Users = () => {
-  const token = localStorage.getItem("token") as string;
-  const { data: tweets, isSuccess, isLoading, isError } = useGetTweetsQuery(token);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    if (isSuccess) {
-      dispatch(setTweets(tweets));
-    }
-  }, [tweets, isSuccess, dispatch]);
-
-  let content: ReactNode;
-
-  if (isLoading) {
-    content = (
-      <CircleLoader
-        size={80}
-        color="#42f"
-      />
-    );
-  }
-
-  if (isError) {
-    content = <h2>Deu erro na requisição dos tweets</h2>;
-  }
-
-  if (isSuccess) {
-    content = tweets.map((tweet) => (
-      <div key={tweet.id}>
-        <div>
-          <h3>{tweet.user.nickname}</h3>
-          <span>{tweet.user.email}</span>
-        </div>
-        <p>{tweet.text}</p>
-      </div>
-    ));
-  }
   return (
     <>
       <h1>Users</h1>
-      {content}
+      <Login />
+      <TweetForm />
     </>
   );
 };
