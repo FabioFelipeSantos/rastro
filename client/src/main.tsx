596dd3b92a7440abbe30d287721cbbd3e58aa0f0
Fábio Santos <fabiotrabmat@gmail.com>
2025-06-06 16:39:48 -0300

    fix: refatoração do main para chamar apenas o app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,14 +4,11 @@ import { Provider } from "react-redux";
 
 import App from "./App.tsx";
 import store from "./store";
-import { ThemeProvider } from "./config/ThemeConfig";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <App />
     </Provider>
   </StrictMode>,
 );
