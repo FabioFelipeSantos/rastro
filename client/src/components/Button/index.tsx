7522fd7df4b3450cc180c33063c19716f3c6e933
Fábio Santos <fabiotrabmat@gmail.com>
2025-06-06 16:52:20 -0300

    feat: atualização do componente Button para incluir suporte ao variant "outline" e ajustes na tipagem de propriedades

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -6,13 +6,15 @@ type ButtonProps = {
   text: string;
   primary?: boolean;
   fullWidth?: boolean;
+  variant?: "default" | "outline";
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button: FC<ButtonProps> = ({ text, primary = true, fullWidth = false, ...props }) => {
+export const Button: FC<ButtonProps> = ({ text, primary = true, fullWidth = false, variant = "default", ...props }) => {
   return (
     <ButtonStyle
       $primary={primary}
       $fullWidth={fullWidth}
+      $variant={variant}
       {...props}
     >
       {text}
--- client/src/components/Button/styles.ts
+++ client/src/components/Button/styles.ts
@@ -1,14 +1,19 @@
 import styled from "styled-components";
 
 type ButtonProps = {
-  $primary?: boolean;
-  $fullWidth?: boolean;
+  $primary: boolean;
+  $fullWidth: boolean;
+  $variant: "default" | "outline";
 };
 
 export const ButtonStyle = styled.button<ButtonProps>`
-  background-color: ${({ theme, $primary }) => ($primary ? theme.colors.primary : theme.colors.secondary)};
-  color: ${({ theme, $primary }) => ($primary ? theme.colors.buttonText : theme.colors.text)};
-  border: 1px solid ${({ theme, $primary }) => ($primary ? theme.colors.primary : theme.colors.border)};
+  background-color: ${({ theme, $primary, $variant }) =>
+    $variant === "outline" ? "transparent" : $primary ? theme.colors.primary : theme.colors.secondary};
+  color: ${({ theme, $primary, $variant }) =>
+    $variant === "outline" ? theme.colors.primary : $primary ? theme.colors.buttonText : theme.colors.text};
+  border: 1px solid
+    ${({ theme, $primary, $variant }) =>
+      $variant === "outline" ? theme.colors.primary : $primary ? theme.colors.primary : theme.colors.border};
   padding: ${({ theme }) => `${theme.spacing(1.2)} ${theme.spacing(2.4)}`};
   border-radius: 9999px;
   font-size: ${({ theme }) => theme.sizing(1.4)};
@@ -19,6 +24,12 @@ export const ButtonStyle = styled.button<ButtonProps>`
 
   &:hover {
     opacity: 0.9;
+    background-color: ${({ theme, $primary, $variant }) =>
+      $variant === "outline"
+        ? `${theme.colors.primary}1A`
+        : $primary
+          ? theme.colors.primary
+          : theme.colors.hoverBackground};
   }
 
   &:disabled {
