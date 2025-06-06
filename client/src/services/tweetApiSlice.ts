24a22cc3df8578640c8c42c1a0dfe2bfd3b5e59c
Fábio Santos <fabiotrabmat@gmail.com>
2025-06-06 16:47:37 -0300

    feat: refatoração do tweetApiSlice para incluir novos tipos e ajustar a lógica de ações de tweet

--- client/src/services/tweetApiSlice.ts
+++ client/src/services/tweetApiSlice.ts
@@ -1,7 +1,19 @@
-import type { TweetCreate } from "../types/tweet";
+import type { Tweet, TweetCreate } from "../types/tweet";
 import { apiSlice } from "./apiSlice";
-import type { TweetListResponse, TweetResponse, TweetStatisticsResponse } from "./types/tweet";
+import type { TweetListResponse, TweetReactionsResponse, TweetResponse, TweetStatisticsResponse } from "./types/tweet";
 import { getHeader } from "../utils/getHeader";
+
+export type TweetActionsArgs = {
+  token: string;
+  tweet_id: number;
+  action: "like" | "dislike" | "share" | "retweet";
+};
+
+export type RetweetArgs = {
+  token: string;
+  tweetId: Tweet["id"];
+  newTweet: TweetCreate;
+};
 
 export const tweetApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
@@ -20,17 +32,54 @@ export const tweetApiSlice = apiSlice.injectEndpoints({
         body: newTweet,
       }),
     }),
-    sendATweetAction: builder.mutation<
-      TweetStatisticsResponse,
-      { token: string; action: "like" | "dislike" | "share" | "retweet" }
-    >({
-      query: ({ token, action }) => ({
-        url: `/tweets/${action}/`,
+    sendATweetAction: builder.mutation<TweetStatisticsResponse, TweetActionsArgs>({
+      query: ({ token, tweet_id, action }) => ({
+        url: `/tweets/${tweet_id}/${action}/`,
         method: "POST",
         headers: getHeader(token),
+      }),
+    }),
+    reactionsByUser: builder.query<TweetReactionsResponse, { tweetId: number; token: string }>({
+      query: ({ tweetId, token }) => ({
+        url: `/tweets/${tweetId}/reacted-by-user`,
+        method: "GET",
+        headers: getHeader(token),
+      }),
+    }),
+    getAllTweets: builder.query<TweetListResponse, string>({
+      query: (token) => ({
+        url: "/tweets/all/",
+        method: "GET",
+        headers: getHeader(token),
+      }),
+    }),
+    getAllAssociatedTweets: builder.query<
+      TweetListResponse,
+      { token: string; tweetId: number; type?: "retweet" | "share" }
+    >({
+      query: ({ token, tweetId, type }) => ({
+        url: `/tweets/${tweetId}/associated-tweets${type ? `?type=${type}` : ""}`,
+        method: "GET",
+        headers: getHeader(token),
+      }),
+    }),
+    addRetweet: builder.mutation<TweetResponse, RetweetArgs>({
+      query: ({ token, tweetId, newTweet }) => ({
+        url: `/tweets/${tweetId}/retweet/`,
+        method: "POST",
+        headers: getHeader(token),
+        body: newTweet,
       }),
     }),
   }),
 });
 
-export const { useGetTweetsQuery, useAddTweetMutation, useSendATweetActionMutation } = tweetApiSlice;
+export const {
+  useGetTweetsQuery,
+  useAddTweetMutation,
+  useSendATweetActionMutation,
+  useReactionsByUserQuery,
+  useGetAllTweetsQuery,
+  useGetAllAssociatedTweetsQuery,
+  useAddRetweetMutation,
+} = tweetApiSlice;
