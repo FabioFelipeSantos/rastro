7ed56b6de2ac7d2c02d5fc43511edec2dfb5e2bf
Fábio Santos <fabiotrabmat@gmail.com>
2025-06-09 19:17:20 -0300

    feat: refatoração do cabeçalho e adição de cards na página inicial, incluindo melhorias de estilo e responsividade

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,39 +1,75 @@
-import { type FC, type ReactNode } from "react";
+import { useState, type FC } from "react";
+import { Menu, MenuOpen } from "@mui/icons-material";
+import { useTheme } from "styled-components";
 
-import { PageHeader, LinksContainer, HeaderLoginButton } from "./styles";
+import * as S from "./styles";
 import { ThemeToggle } from "../ThemeToggle";
 import { Link, useNavigate } from "react-router-dom";
+import rastroLogo from "../../images/logo.png";
 
-type HeaderProps = {
-  main?: boolean;
-};
-
-export const Header: FC<HeaderProps> = ({ main = true }) => {
+export const Header: FC = () => {
   const navigate = useNavigate();
+  const theme = useTheme();
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
-  let content: ReactNode;
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > parseInt(theme.breakpoints.mobile.replace("px", ""))) {
+      setIsMenuOpen(false);
+    }
+  });
 
-  if (main) {
-    content = (
-      <LinksContainer>
-        <Link to="/">Home</Link>
-        <Link to="/">Explorar</Link>
-        <Link to="/">Sobre</Link>
-        <Link to="/">Contato</Link>
-      </LinksContainer>
+  const handleLoginClick = () => {
+    setIsMenuOpen((state) => !state);
+    navigate("/login");
+  };
+
+  const menuContent = () => {
+    return (
+      <S.HeaderContentContainer $isBurgerMenu={isMenuOpen}>
+        <S.LinksContainer onClick={() => setIsMenuOpen((state) => !state)}>
+          <Link to="/">Home</Link>
+          <Link to="/">Explorar</Link>
+          <Link to="/">Sobre</Link>
+          <Link to="/">Contato</Link>
+        </S.LinksContainer>
+        <div className="toggle-button-container">
+          <ThemeToggle />
+        </div>
+        <S.HeaderLoginButton
+          onClick={handleLoginClick}
+          text="Entrar"
+        />
+      </S.HeaderContentContainer>
     );
-  }
+  };
+
+  const burgerMenu = () => {
+    return (
+      <S.BurgerMenuContainer>
+        <S.BurgerMenuStyles onClick={() => setIsMenuOpen((state) => !state)}>
+          {!isMenuOpen ? <Menu /> : <MenuOpen />}
+        </S.BurgerMenuStyles>
+
+        {isMenuOpen && <div>{menuContent()}</div>}
+      </S.BurgerMenuContainer>
+    );
+  };
 
   return (
-    <PageHeader>
-      {content}
-      <div className="toggle-button-container">
-        <ThemeToggle />
-      </div>
-      <HeaderLoginButton
-        onClick={() => navigate("/login")}
-        text="Entrar"
-      />
-    </PageHeader>
+    <>
+      <S.PageHeader>
+        <S.LogoContainer>
+          <img
+            src={rastroLogo}
+            title="Rastro"
+            alt="Rastro logo"
+          />
+        </S.LogoContainer>
+
+        {!isMenuOpen && menuContent()}
+
+        {burgerMenu()}
+      </S.PageHeader>
+    </>
   );
 };
--- client/src/components/Header/styles.ts
+++ client/src/components/Header/styles.ts
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import { Button } from "../Button";
+import { IconButton } from "../IconButton";
 
 export const PageHeader = styled.div`
+  width: 100%;
   padding: ${({ theme }) => `${theme.spacing(1.2)} ${theme.spacing(1.6)}`};
   border-bottom: 1px solid ${({ theme }) => theme.colors.border};
   font-size: ${({ theme }) => theme.sizing(1)};
@@ -13,14 +15,40 @@ export const PageHeader = styled.div`
   z-index: 10;
 
   display: flex;
-  justify-content: end;
+  justify-content: space-between;
 
   .toggle-button-container {
     margin: ${({ theme }) => `0 ${theme.spacing(2)}`};
+
+    @media screen and (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+      margin: 16px 0;
+    }
   }
+`;
+
+type HeaderContentContainerProps = {
+  $isBurgerMenu: boolean;
+};
+
+export const HeaderContentContainer = styled.div<HeaderContentContainerProps>`
+  width: 80%;
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+  align-items: center;
+  transition: all 0.2s ease;
 
   @media screen and (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
-    display: none;
+    width: fit-content;
+    display: ${({ $isBurgerMenu }) => ($isBurgerMenu ? "flex" : "none")};
+    flex-direction: column;
+    position: absolute;
+    right: 0;
+    top: 110%;
+    padding: 12px;
+    border-radius: 8px;
+    border: 1px solid ${({ theme }) => theme.colors.border};
+    background-color: ${({ theme }) => theme.applyAlpha(theme.colors.cardBackground, 0.98)};
   }
 `;
 
@@ -30,10 +58,50 @@ export const LinksContainer = styled.div`
   align-items: center;
   gap: ${({ theme }) => theme.spacing(2)};
   font-size: ${({ theme }) => theme.sizing(1.4)};
+
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    gap: ${({ theme }) => theme.spacing(1.4)};
+  }
+
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    flex-direction: column;
+  }
 `;
 
 export const HeaderLoginButton = styled(Button)`
-  padding: ${({ theme }) => `0 ${theme.spacing(2.6)}`};
+  padding: ${({ theme }) => `4px ${theme.spacing(2.6)}`};
   font-size: ${({ theme }) => theme.sizing(1.4)};
   font-weight: 400;
+
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+  }
+`;
+
+export const LogoContainer = styled.div`
+  max-width: 120px;
+  width: 100%;
+
+  & > img {
+    width: 90%;
+  }
+
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    display: none;
+  }
+`;
+
+export const BurgerMenuContainer = styled.div`
+  position: relative;
+  width: 100%;
+  display: none;
+  justify-content: end;
+
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    display: flex;
+  }
+`;
+
+export const BurgerMenuStyles = styled(IconButton)`
+  background-color: ${({ theme }) => theme.applyAlpha(theme.colors.primary, 0.1)};
+  padding: 6px;
 `;
