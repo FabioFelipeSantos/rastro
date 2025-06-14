0e7ded0e7477b7602460b427cb7440f03fbdf32b
Fábio Santos <fabiotrabmat@gmail.com>
2025-06-14 18:41:16 -0300

    feat: adiciona permissões sudo para comandos de cópia e remoção no script de instalação

--- start.sh
+++ start.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
+
+set -e
+
 echo "Começando a instalação..."
 
 echo "Instalando configuração do Apache..."
-cp apache-vhost.conf /etc/apache2/sites-available/000-default.conf
+sudo cp apache-vhost.conf /etc/apache2/sites-available/000-default.conf
 
 echo "Rodando build do client..."
 cd client/
 pnpm install
 pnpm build
+cd ..
 
 echo "Removendo arquivos antigos do servidor e copiando os novos..."
-rm -rf /var/www/html/*
-cp -r dist/* /var/www/html/
+sudo rm -rf /var/www/html/*
+sudo cp -r client/dist/* /var/www/html/
 
 echo "Instalando dependências da API..."
 cd ../server/
-$(poetry env activate)
 poetry install --no-root
 poetry run python manage.py collectstatic --noinput
 
-deactivate
 gunicorn twitter_api.wsgi
