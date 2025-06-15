b1616fdf0503d63b5597d76942d1573775d84598
Fábio Santos <fabiotrabmat@gmail.com>
2025-06-15 18:41:32 -0300

    feat: adicionando script de deploy automático

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+echo ">>> Iniciando deploy automático do projeto"
+
+cd $HOME/app/
+
+echo ">>> Baixando atualizações do repositório remoto ..."
+
+git checkout main
+git fetch origin main
+git reset --hard origin/main
+
+echo ">>> Instalando dependências do projeto ..."
+./start.sh
+
+echo ">>> Deploy automático concluído com sucesso!"
