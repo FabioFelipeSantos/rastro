762a0b6f7f8a5f709ef50a96e10a3dca8c27311f
Fábio Santos <fabiotrabmat@gmail.com>
2025-06-17 15:14:59 -0300

    refactor: simplificando a função getImageUrl removendo código comentado e variáveis não utilizadas

--- client/src/utils/getImageUrl.ts
+++ client/src/utils/getImageUrl.ts
@@ -1,14 +1,9 @@
-const API_URL = "http://localhost:8000/";
-
 export const getImageUrl = (path: string): string => {
   if (!path) {
     return "https://placehold.co/100x100/CCCCCC/FFFFFF?text=?";
-  }
-
-  if (path.startsWith("http://") || path.startsWith("https://")) {
+  } else {
+    // if (path.startsWith("http://") || path.startsWith("https://")) {
+    // }
     return path;
   }
-
-  const cleanPath = path.replace(/^\/+|\/+$/g, "");
-  return `${API_URL}${cleanPath}`;
 };
