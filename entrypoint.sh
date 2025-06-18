df861334d92cb3f962bcf9f52a44548a9d7f2528
Fábio Santos <fabiotrabmat@gmail.com>
2025-06-18 10:24:05 -0300

    fix: corrigir erro de digitação na variável DJANGO_CSFR_TRUSTED_ORIGINS e adicionar arquivo de serviço do Gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-# Para o script se qualquer comando falhar
 set -e
 
 echo ">>> Rodando migrações do banco de dados..."
-# Usamos o poetry run para executar o manage.py
 poetry run python manage.py migrate
 
 echo ">>> Rodando o seeder do banco de dados..."
